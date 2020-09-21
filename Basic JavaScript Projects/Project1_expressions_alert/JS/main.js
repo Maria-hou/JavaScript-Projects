3815b56589215ee7892b258aba8956e1af5ae197
Maria Hou <maria.hou@yahoo.com>
2020-09-20 23:27:33 -0400

    Commit main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+document.write("Hello, world! ");    //Printing simple expressions
+var A = "This is a string ";         // Assigning variable
+document.write(A);                   // Print variable A
+
+var todayDate = "Today's date is September 20th.";   // Create new variable
+window.alert(todayDate);                             // Print variable
+
+// Concatenate strings and print them out
+document.write("Johnny said: \"I did not hit her, it's not true!" +
+ "It\'s bullshit! I did not hit her! I did not! Oh hi, Mark.\"");
+
+// Concatenate strings as a variable then print it out
+var B = "That is one of the most" + " famous lines from \"The Room\".";
+document.write(B);
+
+// Create multiple variables in one declaration and assign colors to them
+var student = "Tom Smith", GPA = "3.7", major = "Computer Science", age = "21";
+var student = student.fontcolor("green");
+var GPA = GPA.fontcolor("red");
+var major = major.fontcolor("purple");
+var age = age.fontcolor("blue");
+
+// Print out variable GPA
+document.write(GPA);
+
+(3+3);  //An expression
